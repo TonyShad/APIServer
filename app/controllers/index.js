d8cf6c7d3e385d290bbbc2b984dfa8ef036a946c
Maxim <McSim1993@gmail.com>
2017-03-12 21:45:58 +0300

    Controller fix

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,31 +2,31 @@ const Error = require('common').Error;
 const errorCodes = require('common/data/errorCodes.json');
 
 class Controller {
-	constructor(req) {
-		this.req = req;
-		this._actions = this.prototype.actions || {};
-	}
+    constructor(req) {
+        this.req = req;
+        this._actions = this.actions || {};
+    }
 
-	callAction(name) {
-		return new Promise((resolve, reject) => {
-		    const action = this.getAction(name);
-		    return action.call(this);
-		});
-	}
+    callAction(name) {
+        return new Promise((resolve) => {
+            const action = this.getAction(name);
+            resolve(action.call(this));
+        });
+    }
 
-	getAction(name) {
-		let result = this._actions[name];
-		if (!result) {
-		    throw new Error(errorCodes.notFound, 'Action not found', { actionName: name });
-		}
-		return result;
-	}
-
-	static registerAction(name, fn) {
-		this.prototype.actions = this.prototype.actions || {};
-		this.prototype.actions[name] = fn;
-		return this;
-	} 
+    getAction(name) {
+        let result = this._actions[name];
+        if (!result) {
+            throw new Error(errorCodes.notFound, 'Action not found', {actionName: name});
+        }
+        return result;
+    }
 }
+
+Controller.registerAction = function (name, fn) {
+    this.prototype.actions = this.prototype.actions || {};
+    this.prototype.actions[name] = fn;
+    return this;
+};
 
 module.exports = Controller;
